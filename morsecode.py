0d6ebc594ad657a532d281429b0103676d1871fe
chws <rebeccachoosir@gmail.com>
2021-01-21 11:26:58 +0900

    is validated morse code

--- morsecode.py
+++ morsecode.py
@@ -109,12 +109,13 @@ def is_validated_morse_code(user_input):
         >>> mc.is_validated_morse_code("!.1 abc --")
         False
     """
-    # ===Modify codes below=============
-    # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
-    result = None
-
-    return result
-    # ==================================
+    if not user_input.translate(str.maketrans("-.", "  ")).replace(' ', '') == '':
+        return False
+    morse_code_dict = get_morse_code_dict()
+    for code in user_input.split():
+        if code not in morse_code_dict.values():
+            return False
+    return True
 
 
 def get_cleaned_english_sentence(raw_english_sentence):
